e8ff3e747143d13f371536758d901e5f145bcf1a
Sebastian Fernandez <sebastian.fernandez@hardis.fr>
2020-03-22 11:19:14 +0100

    JS: DFS and BFS

---
+++ BFS/BFS.js
@@ -0,0 +1,13 @@
+var dfs = (start) => {
+  var listToExplore = [ start ];
+  nodes[ start ].visited = true;
+  while ( listToExplore.length > 0 ) {
+    var nodeIndex = listToExplore.pop();
+    nodes[ nodeIndex ].links.forEach( function( childIndex ) {
+      if ( !nodes[ childIndex ].visited ) {
+        nodes[ childIndex ].visited = true;
+        listToExplore.push( childIndex );
+      }
+    });
+  }
+};
